d2e51e51b4109be68e9dc6b7fd7a1d7b83b595d0
osipov <osipov@komus.net>
2024-01-17 18:37:40 +0500

    Initial commit

---
+++ src/main/java/com/project/scrabble/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.project.scrabble.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello from Spring Boot Web";
+    }
+}
---
+++ src/main/java/com/project/scrabble/controller/MainDB.java
@@ -0,0 +1,5 @@
+package com.project.scrabble.controller;
+
+
+public class MainDB {
+}
